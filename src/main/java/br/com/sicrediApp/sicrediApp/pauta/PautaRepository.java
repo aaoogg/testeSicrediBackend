a106e0706f46090aef09e3044f6bc55748f95579
antonio <antoniobpt@gmail.com>
2024-06-14 01:05:59 -0300

    Mensagem descritiva das alterações

--- src/main/java/br/com/sicrediApp/sicrediApp/pauta/PautaRepository.java
+++ src/main/java/br/com/sicrediApp/sicrediApp/pauta/PautaRepository.java
@@ -1,6 +1,10 @@
 package br.com.sicrediApp.sicrediApp.pauta;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PautaRepository extends JpaRepository<Pauta, Long> {
+    Optional<Pauta> findByNome(String nome);
+    boolean existsByNome(String nome);
 }
